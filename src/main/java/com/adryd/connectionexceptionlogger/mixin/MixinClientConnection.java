f8a1fd43a0f22561e5aa5e12eaccaebbcb5bfcc7
adryd <me@adryd.com>
2023-06-18 03:33:09 -0400

    Initial Commit

---
+++ src/main/java/com/adryd/connectionexceptionlogger/mixin/MixinClientConnection.java
@@ -0,0 +1,19 @@
+package com.adryd.connectionexceptionlogger.mixin;
+
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.network.ClientConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientConnection.class)
+public class MixinClientConnection {
+    private final Logger LOGGER = LoggerFactory.getLogger("ConnectionExceptionLogger");
+    @Inject(at = @At("HEAD"), method = "exceptionCaught")
+    private void logExceptionStackTrace(ChannelHandlerContext context, Throwable ex, CallbackInfo ci) throws Throwable {
+        LOGGER.error("", ex);
+    }
+}
