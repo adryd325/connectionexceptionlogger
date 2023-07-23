50c4a997fac2e7b97d9d99baf0f733b124cdf377
adryd <me@adryd.com>
2023-07-22 20:23:43 -0400

    Update MixinClientConnection.java

--- src/main/java/com/adryd/connectionexceptionlogger/mixin/MixinClientConnection.java
+++ src/main/java/com/adryd/connectionexceptionlogger/mixin/MixinClientConnection.java
@@ -5,9 +5,9 @@ import net.minecraft.network.ClientConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Unique;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
